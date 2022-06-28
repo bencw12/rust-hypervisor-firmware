343a771a363e07d4305b04947dfdf2d688107cbc
Benjamin Holmes <bholmes@vassar.edu>
2022-06-28 14:02:34 +0000

    Check if SEV is enabled and set C-bit during page table setup

--- src/paging.rs
+++ src/paging.rs
@@ -15,30 +15,40 @@ static mut L4_TABLE: PageTable = PageTable::new();
 static mut L3_TABLE: PageTable = PageTable::new();
 #[no_mangle]
 static mut L2_TABLES: [PageTable; ADDRESS_SPACE_GIB] = [TABLE; ADDRESS_SPACE_GIB];
+#[no_mangle]
+static mut SEV_ENC_BIT: [u64; 1] = [0];
 
 pub fn setup() {
     // SAFETY: This function is idempontent and only writes to static memory and
     // CR3. Thus, it is safe to run multiple times or on multiple threads.
     let (l4, l3, l2s) = unsafe { (&mut L4_TABLE, &mut L3_TABLE, &mut L2_TABLES) };
     log!("Setting up {} GiB identity mapping", ADDRESS_SPACE_GIB);
+    let sev_enc_bit = unsafe { SEV_ENC_BIT[0] };
+    if sev_enc_bit > 0 {
+        log!("SEV Enabled: 0x{:x}", sev_enc_bit);
+    }
     let pt_flags = PageTableFlags::PRESENT | PageTableFlags::WRITABLE;
-
     // Setup Identity map using L2 huge pages
     let mut next_addr = PhysAddr::new(0);
     for l2 in l2s.iter_mut() {
         for l2e in l2.iter_mut() {
-            l2e.set_addr(next_addr, pt_flags | PageTableFlags::HUGE_PAGE);
+            let addr = PhysAddr::new(next_addr.as_u64() | sev_enc_bit );
+            l2e.set_addr(addr, pt_flags | PageTableFlags::HUGE_PAGE);
             next_addr += Size2MiB::SIZE;
         }
     }
 
     // Point L3 at L2s
     for (i, l2) in l2s.iter().enumerate() {
-        l3[i].set_addr(phys_addr(l2), pt_flags);
+        let addr = phys_addr(l2).as_u64() | sev_enc_bit;
+        let addr = PhysAddr::new(addr);
+        l3[i].set_addr(addr, pt_flags);
     }
 
     // Point L4 at L3
-    l4[0].set_addr(phys_addr(l3), pt_flags);
+    let addr = phys_addr(l3).as_u64() | sev_enc_bit;
+    let addr = PhysAddr::new(addr);
+    l4[0].set_addr(addr, pt_flags);
 
     // Point Cr3 at L4
     let (cr3_frame, cr3_flags) = Cr3::read();
